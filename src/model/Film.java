48d826ee76c9c6f8ab0c2c42ce9d934d6180dd07
HP <HP@Beelzebub>
2023-08-02 13:59:44 +0700

    Feat(Film.java): Added Variable, Constructor and Getter Setter

--- src/model/Film.java
+++ src/model/Film.java
@@ -12,11 +12,44 @@ package model;
 public class Film{
     
 //    bikin variable pake private
-    
+        private int id;
+        private String film_title;
+        private String age_category;
+        
     
     // bikin constructor pake variable diatas
-    
+        public Film(int id, String film_title, String age_category){
+            this.id = id;
+            this.film_title = film_title;
+            this.age_category = age_category;
+        }
 //    bikin getter setter 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFilm_title() {
+        return film_title;
+    }
+
+    public void setFilm_title(String film_title) {
+        this.film_title = film_title;
+    }
+
+    public String getAge_category() {
+        return age_category;
+    }
+
+    public void setAge_category(String age_category) {
+        this.age_category = age_category;
+    }
+        
+        
 }
 
     
